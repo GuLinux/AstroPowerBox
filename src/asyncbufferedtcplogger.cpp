c8603b26c5db44bd9a4cc89c430a8b871b7205da
Marco Gulino <marco.gulino@gmail.com>
2024-09-23 12:36:43 +0100

    Add backlog to buffered logger

--- src/asyncbufferedtcplogger.cpp
+++ src/asyncbufferedtcplogger.cpp
@@ -1,19 +1,29 @@
 #include "asyncbufferedtcplogger.h"
+#include "configuration.h"
 
 APB::AsyncBufferedTCPLogger::AsyncBufferedTCPLogger(AsyncServer & loggerServer) {
     loggerServer.onClient([this](void *,AsyncClient *c){
       this->client = c;
+      if(!this->backlog.empty()) {
+        c->write("==== Flushing backlog ====\n");
+        while(!this->backlog.empty()) {
+          c->write(this->backlog.front().c_str(), this->backlog.front().length());
+          this->backlog.pop();
+        }
+        c->write("==== Backlog finished ====\n");
+      }
     }, nullptr);
 }
 
+#include "utils.h"
 size_t APB::AsyncBufferedTCPLogger::write(uint8_t c) {
-    if(!client) {
-      return 0;
-    }
     buffer[currentPosition++] = c;
     if(c == '\n') {
+      ScopeGuard resetBuffer(std::bind(&AsyncBufferedTCPLogger::reset, this));
+      if(!client) {
+        return fillBacklog();
+      }
       client->write(buffer.data(), currentPosition);
-      reset();
     }
     return 1;
 }
@@ -21,4 +31,14 @@ size_t APB::AsyncBufferedTCPLogger::write(uint8_t c) {
 void APB::AsyncBufferedTCPLogger::reset() {
     std::fill(std::begin(buffer), std::end(buffer), 0);
     currentPosition = 0;
+}
+
+size_t APB::AsyncBufferedTCPLogger::fillBacklog() {
+  #if APB_NETWORK_LOGGER_BACKLOG > 0
+    backlog.push(String{buffer.data()});
+    while(backlog.size() > APB_NETWORK_LOGGER_BACKLOG) {
+      backlog.pop();
+    }
+  #endif
+    return 0;
 }
