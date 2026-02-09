b664b1f7bf22b09b18c4330c66b89531a3cc50bc
Marco Gulino <marco.gulino@gmail.com>
2026-02-09 01:20:36 +0000

    Add fallback for undefined FAN pin

--- esp32/src/fan.cpp
+++ esp32/src/fan.cpp
@@ -15,6 +15,7 @@ Fan &Fan::Instance()
 
 void Fan::setup(uint32_t frequency, float duty)
 {
+#ifdef APB_PWM_FAN_PIN
     pinMode(APB_PWM_FAN_PIN, OUTPUT);
     for(channel=0; channel < 16; ++channel) {
         if(ledcSetup(channel, frequency, 8) != 0) {
@@ -26,18 +27,27 @@ void Fan::setup(uint32_t frequency, float duty)
     }
     ledcAttachPin(APB_PWM_FAN_PIN, channel); 
     setDuty(duty);
+#endif
 }
 
 void Fan::setDuty(float duty) {
+#ifdef APB_PWM_FAN_PIN
     _duty = duty;
     int analogValue = static_cast<int>(duty * 255.0);
     Log.infoln(LOG_SCOPE "Setting fan duty to %F (analog value %d) to channel %d", duty, analogValue, channel);
     ledcWrite(channel, analogValue);
+#endif
 }
 float Fan::duty() const {
+#ifdef APB_PWM_FAN_PIN
     return _duty;
+#else
+    return 0;
+#endif
 }
 
 Fan::~Fan() {
+#ifdef APB_PWM_FAN_PIN
     ledcWrite(channel, 0);
+#endif
 }
