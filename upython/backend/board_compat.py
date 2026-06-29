48c76d2d11375420b673e81a6492e720fff576d5
Marco Gulino <marco.gulino@gmail.com>
2026-06-29 21:20:52 +0100

    Split options for gpio and wifi simulators

--- upython/backend/board_compat.py
+++ upython/backend/board_compat.py
@@ -16,12 +16,18 @@ elif sys.implementation.name == 'cpython':
     import os
     import asyncio
     from boards.cpython.json_config_storage import JsonConfigStorage as ConfigStorage
-    if os.environ.get('SIMULATOR', '0') == '1':
+    if os.environ.get('SIMULATOR_WIFI', '0') == '1':
         from boards.simulator.wifi_manager import SimulatorWiFiManager as WiFiManager
+    else:
+        from boards.cpython.wifi_manager import WiFiManager
+    if os.environ.get('SIMULATOR_GPIO', '0') == '1':
         import boards.simulator.gpio as gpio
+    else:
+        pass
+        
+        
     pinout_config_path = os.environ.get('PINOUT_CONFIG_PATH', 'config_files/pinout.json')
     server_port = int(os.environ.get('PORT', '80'))
     server_debug = os.environ.get('DEBUG', 0) in ['1', 'true']
 else:
     raise RuntimeError(f'Unsupported python platform: {sys.implementation.name}')
-
