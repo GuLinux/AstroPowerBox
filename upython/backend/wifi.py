5978a3619ccb33fa97e7a982802aec69912e6d4c
Marco Gulino <marco.gulino@gmail.com>
2026-07-30 17:30:00 +0100

    Refactor type hints in WiFi class for consistency and clarity

--- upython/backend/wifi.py
+++ upython/backend/wifi.py
@@ -1,46 +1,45 @@
-from __future__ import annotations # see https://stackoverflow.com/questions/33533148/how-do-i-type-hint-a-method-with-the-type-of-the-enclosing-class
 from utils import check_required_keys
 
 
 class WiFi:
-    def __init__(self, ssid: str, psk: str | None = None):
+    def __init__(self, ssid: 'str', psk: 'str | None' = None):
         self._ssid = ssid
         self._psk = psk
 
     @property
-    def ssid(self) -> str:
+    def ssid(self) -> 'str':
         return self._ssid
 
     @ssid.setter
-    def ssid(self, ssid: str) -> None:
+    def ssid(self, ssid: 'str') -> 'None':
         self._ssid = ssid
 
     @property
-    def psk(self) -> str | None:
+    def psk(self) -> 'str | None':
         return self._psk
 
     @psk.setter
-    def psk(self, psk: str) -> None:
+    def psk(self, psk: 'str | None') -> 'None':
         self._psk = psk
 
     @property
-    def json(self) -> dict:
+    def json(self) -> 'dict':
         return {
             'ssid': self.ssid,
             'psk': self.psk,
         }
 
     @classmethod
-    def from_json(cls, json: dict) -> WiFi:
+    def from_json(cls, json: 'dict') -> 'WiFi':
         check_required_keys(['ssid'], json)
-        return WiFi(json['ssid'], json.get('psk', ''))
+        return WiFi(json['ssid'], json.get('psk', None))
 
     @classmethod
-    def from_json_list(cls, json: list) -> list[WiFi]:
+    def from_json_list(cls, json: 'list') -> 'list[WiFi]':
         return [WiFi.from_json(wifi) for wifi in json]
 
     @classmethod
-    def to_json_list(cls, json: list) -> list[dict]:
+    def to_json_list(cls, json: 'list') -> 'list[dict]':
         return [wifi.json for wifi in json]
 
     def __str__(self):
